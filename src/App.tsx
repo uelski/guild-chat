d00d4f9fa1c2b749a2fe2207283ef984472d58b0
Sam Vredenburgh <svredenburgh@SVredenburgh-CHI-2021-MBP16.local>
2021-08-02 20:43:47 -0600

    start of building chat and home components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './components/Home'
+import Chat from './components/Chat'
 
 function App() {
   return (
     <div className="App">
 
+      <Router>
+        <Switch>
+          <Route path="/users/:id" component={Chat}/>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
